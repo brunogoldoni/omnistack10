57c45b9e7013805c5f8dd3ebf86863d38927b256
bgoldoni <bgoldoni@orion.tech>
2020-01-17 11:29:19 -0300

    Building the interface web

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const routes = require('./routes')
 
+const cors = require('cors');
+
 const app = express();
 
 mongoose.connect('mongodb://bruno:omnistack@heineken-shard-00-00-mlhas.mongodb.net:27017,heineken-shard-00-01-mlhas.mongodb.net:27017,heineken-shard-00-02-mlhas.mongodb.net:27017/week10?ssl=true&replicaSet=Heineken-shard-0&authSource=admin&retryWrites=true&w=majority', {
@@ -9,6 +11,7 @@ mongoose.connect('mongodb://bruno:omnistack@heineken-shard-00-00-mlhas.mongodb.n
   useUnifiedTopology: true
 });
 
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
