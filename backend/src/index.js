7a4da29a0516e99611824c124f16b837013cb8f0
bgoldoni <bgoldoni@orion.tech>
2020-01-22 09:27:03 -0300

    advanced features

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const mongoose = require('mongoose');
-const routes = require('./routes')
-
 const cors = require('cors');
+const http = require('http');
+
+const routes = require('./routes')
+const { setupWebsocket } = require('./websocket')
 
 const app = express();
+const server = http.Server(app);
+
+setupWebsocket(server);
 
 mongoose.connect('mongodb://bruno:omnistack@heineken-shard-00-00-mlhas.mongodb.net:27017,heineken-shard-00-01-mlhas.mongodb.net:27017,heineken-shard-00-02-mlhas.mongodb.net:27017/week10?ssl=true&replicaSet=Heineken-shard-0&authSource=admin&retryWrites=true&w=majority', {
   useNewUrlParser: true,
@@ -15,4 +20,4 @@ app.use(cors());
 app.use(express.json());
 app.use(routes);
 
-app.listen(3333);
+server.listen(3333);
